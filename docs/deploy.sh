a2e897bf24aa0d9cc699c783f0bdcddefbb3de3d
adamws <adamws@users.noreply.github.com>
2021-02-18 22:43:15 +0100

    add github worflow for docs deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env sh
 
+# Build and deploy documentation with github actions
+
 # abort on errors
 set -e
 
-# build
+# install and build
+echo "==> Dependencies install\n"
+npm install
+
+echo "==> Build\n"
 npm run build
 
 # navigate into the build output directory
 cd src/.vuepress/dist
 
+echo "==> Prepare to deploy\n"
 git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
+if [ -z "$(git status --porcelain)" ]; then
+    echo "Something went wrong" && \
+    echo "Exiting..."
+    exit 0
+fi
+
+echo "==> Start deploying"
 git add -A
-git commit -m 'deploy'
+git commit -m 'Deploy documentation'
 
-git push -f git@github.com:adamws/keyboard-tools.git master:gh-pages
+DEPLOY_REPO="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+git push --force $DEPLOY_REPO master:gh-pages
 
-cd -
+rm -fr .git
+
+cd $GITHUB_WORKSPACE
+echo "==> Deploy succeeded"
