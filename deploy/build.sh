d55954d5d6cd9b35d56c29ed634d960f4c2d94a5
adamws <adamws@users.noreply.github.com>
2021-04-11 23:30:30 +0200

    enable push to dockerhub

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,8 +7,8 @@ set -o pipefail
 build_and_push () {
     docker build -t admwscki/$1:$2 -f $3 $4
     docker tag admwscki/$1:$2 admwscki/$1:latest
-    #docker push admwscki/$1:$2
-    #docker push admwscki/$1:latest
+    docker push admwscki/$1:$2
+    docker push admwscki/$1:latest
 }
 
 build_and_push keyboard-tools-server $TAG deploy/Dockerfile .
