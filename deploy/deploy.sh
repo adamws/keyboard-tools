08aadcb647820f0bf0b38ee595e4e6d4a5dd2962
adamws <adamws@users.noreply.github.com>
2021-04-14 23:10:42 +0200

    deploy application on remote server with circleci

---
+++ deploy/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+if [ -z "$(ssh-keygen -F $SSH_HOST)" ]; then
+    echo "Adding host to known hosts"
+    ssh-keyscan -H $SSH_HOST >> ~/.ssh/known_hosts 2> /dev/null
+fi
+
+scp deploy/docker-compose.yml deploy/production.env $SSH_USER@$SSH_HOST:/home/$SSH_USER/app
+ssh $SSH_USER@$SSH_HOST <<'EOL'
+    cd app
+    docker-compose up -d
+EOL
