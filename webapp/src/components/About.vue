eaee40b07e23dc35ae5e1d93082f4104f34f4579
adamws <adamws@users.noreply.github.com>
2023-01-01 21:10:45 +0100

    Add donation information

--- webapp/src/components/About.vue
+++ webapp/src/components/About.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    All tools on this site are free of charge and open sourced.
-    For details check out <a href="https://github.com/adamws/keyboard-tools">github</a> project page.
+    <p>
+      All tools on this site are free of charge and open sourced. That said, you can support this project
+      by donating at <a href="https://ko-fi.com/adamws">ko-fi/adamws</a>.
+    </p>
+    <p>
+      For details check out <a href="https://github.com/adamws/keyboard-tools">github</a> page.
+    </p>
   </div>
 </template>
 
