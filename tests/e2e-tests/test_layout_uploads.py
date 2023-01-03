166d05936580697ea6601eaf461193cca22d5a28
adamws <adamws@users.noreply.github.com>
2023-01-03 22:42:40 +0100

    Add more option combinations to generate test

--- tests/e2e-tests/test_layout_uploads.py
+++ tests/e2e-tests/test_layout_uploads.py
@@ -10,13 +10,30 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
+@pytest.mark.parametrize("layout", ["2x2", "arisu"])
 @pytest.mark.parametrize(
-        "layout", ["2x2", "arisu"]
+    "library", ["perigoso/keyswitch-kicad-library", "ai03-2725/MX_Alps_Hybrid"]
 )
-def test_correct_layout(layout, selenium, request, download_dir):
+@pytest.mark.parametrize("footprint", ["MX", "Alps", "MX/Alps Hybrid"])
+@pytest.mark.parametrize("routing", ["Disabled", "Full"])
+@pytest.mark.parametrize("controller_circuit", ["None", "ATmega32U4"])
+def test_correct_layout_no_matrix_predefined(
+    layout,
+    library,
+    footprint,
+    routing,
+    controller_circuit,
+    selenium,
+    request,
+    download_dir,
+):
     filename = request.module.__file__
     test_dir, _ = os.path.splitext(filename)
     layout_file = f"{test_dir}/{layout}.json"
+
+    for option in [library, footprint, routing, controller_circuit]:
+        span = selenium.find_element("xpath", f"//span[text()='{option}']")
+        span.click()
 
     input_file = selenium.find_element("xpath", "//input[@id='file']")
     selenium.execute_script(
