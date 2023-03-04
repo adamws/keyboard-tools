dc9ba16aad43f43ebcff3c516016cbdf384b3adb
adamws <adamws@users.noreply.github.com>
2023-03-04 14:37:16 +0100

    Simplify and format About and Help components

--- webapp/src/components/About.vue
+++ webapp/src/components/About.vue
@@ -1,21 +1,17 @@
+<script setup></script>
+
 <template>
   <div>
     <p>
-      All tools on this site are free of charge and open sourced. That said, you can support this project
-      by donating at <a href="https://ko-fi.com/adamws">ko-fi/adamws</a>.
+      All tools on this site are free of charge and open sourced. That said, you
+      can support this project by donating at
+      <a href="https://ko-fi.com/adamws">ko-fi/adamws</a>.
     </p>
     <p>
-      For details check out <a href="https://github.com/adamws/keyboard-tools">github</a> page.
+      For details check out
+      <a href="https://github.com/adamws/keyboard-tools">github</a> page.
     </p>
   </div>
 </template>
 
-<script>
-export default {
-  // eslint-disable-next-line
-  name: 'About'
-};
-</script>
-
-<style scoped>
-</style>
+<style scoped></style>
--- webapp/src/components/Help.vue
+++ webapp/src/components/Help.vue
@@ -1,15 +1,12 @@
+<script setup></script>
+
 <template>
   <div>
-    Project documentation is hosted at <a href="https://adamws.github.io/keyboard-tools">https://adamws.github.io/keyboard-tools</a>
+    Project documentation is hosted at
+    <a href="https://adamws.github.io/keyboard-tools"
+      >https://adamws.github.io/keyboard-tools</a
+    >
   </div>
 </template>
 
-<script>
-export default {
-  // eslint-disable-next-line
-  name: 'Help'
-};
-</script>
-
-<style scoped>
-</style>
+<style scoped></style>
