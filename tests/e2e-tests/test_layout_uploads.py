674143c3b056a7dbb168e77f8d9c28acc659519c
adamws <adamws@users.noreply.github.com>
2023-08-19 18:06:29 +0200

    Add test of layout with various size keys

--- tests/e2e-tests/test_layout_uploads.py
+++ tests/e2e-tests/test_layout_uploads.py
@@ -16,6 +16,19 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+def assert_zip_content(zipfile):
+    files_in_zip = zipfile.namelist()
+    assert "logs/kbplacer.log" in files_in_zip
+    expected_in_keyboard_dir = [
+        "sym-lib-table",
+        "keyboard.net",
+        "keyboard.kicad_pro",
+        "keyboard.kicad_pcb",
+    ]
+    for name in expected_in_keyboard_dir:
+        assert f"keyboard/{name}" in files_in_zip
+
+
 def assert_kicad_log(log_file, layout_file):
     layout = None
     with open(layout_file) as f:
@@ -43,6 +56,55 @@ def assert_kicad_log(log_file, layout_file):
     assert next_key == number_of_keys + 1
 
 
+def upload_layout(selenium, layout_file):
+    input_file = selenium.find_element("xpath", "//input[@id='file']")
+    input_file.send_keys(layout_file)
+    logger.info("Layout uploaded, started PCB generation")
+
+
+def wait_and_download(selenium, download_dir):
+    button = WebDriverWait(selenium, 60).until(
+        ec.any_of(
+            ec.element_to_be_clickable((By.XPATH, "//button[@id='download-btn']")),
+            ec.element_to_be_clickable(
+                (By.XPATH, "//*[@class='el-message-box__btns']/button")
+            ),
+        )
+    )
+    assert "download-btn" in button.get_attribute("id")
+
+    logger.info("PCB done, downloading")
+    button.click()
+
+    download_link = selenium.find_element("xpath", "//a[@id='download']")
+    link = download_link.get_attribute("href")
+
+    job_id = re.search(f"{selenium.current_url}api/pcb/(.*)/result", link).group(1)
+
+    download_file_path = f"{download_dir}/{job_id}.zip"
+
+    timeout = 60
+    mustend = time.time() + timeout
+    while time.time() < mustend:
+        if os.path.exists(download_file_path):
+            break
+        time.sleep(1)
+    logger.info("Download done")
+    assert os.path.isfile(download_file_path)
+
+    return download_file_path
+
+
+def layout_test_steps(selenium, layout_file, download_dir):
+    upload_layout(selenium, layout_file)
+    download_file = wait_and_download(selenium, download_dir)
+
+    with zipfile.ZipFile(download_file, "r") as result:
+        assert_zip_content(result)
+        with result.open("logs/kbplacer.log") as log_file:
+            assert_kicad_log(log_file, layout_file)
+
+
 @pytest.mark.parametrize("layout", ["2x2", "arisu"])
 @pytest.mark.parametrize("footprint", ["MX", "Alps", "MX/Alps Hybrid"])
 @pytest.mark.parametrize("routing", ["Disabled", "Full"])
@@ -64,54 +126,15 @@ def test_correct_layout_no_matrix_predefined(
         span = selenium.find_element("xpath", f"//span[contains(.,'{option}')]")
         span.click()
 
-    input_file = selenium.find_element("xpath", "//input[@id='file']")
-    input_file.send_keys(layout_file)
-    logger.info("Layout uploaded, started PCB generation")
+    layout_test_steps(selenium, layout_file, download_dir)
 
-    button = WebDriverWait(selenium, 60).until(
-        ec.any_of(
-            ec.element_to_be_clickable((By.XPATH, "//button[@id='download-btn']")),
-            ec.element_to_be_clickable(
-                (By.XPATH, "//*[@class='el-message-box__btns']/button")
-            ),
-        )
-    )
-    assert "download-btn" in button.get_attribute("id")
 
-    logger.info("PCB done, downloading")
-    button.click()
+def test_layout_with_various_key_sizes(selenium, request, download_dir):
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+    layout_file = f"{test_dir}/sizes.json"
 
-    download_link = selenium.find_element("xpath", "//a[@id='download']")
-    link = download_link.get_attribute("href")
-
-    job_id = re.search(f"{selenium.current_url}api/pcb/(.*)/result", link).group(1)
-
-    download_file = f"{download_dir}/{job_id}.zip"
-
-    timeout = 60
-    mustend = time.time() + timeout
-    while time.time() < mustend:
-        if os.path.exists(download_file):
-            break
-        time.sleep(1)
-    logger.info("Download done")
-
-    assert os.path.isfile(download_file)
-
-    with zipfile.ZipFile(download_file, "r") as result:
-        files_in_zip = result.namelist()
-        assert "logs/kbplacer.log" in files_in_zip
-        expected_in_keyboard_dir = [
-            "sym-lib-table",
-            "keyboard.net",
-            "keyboard.kicad_pro",
-            "keyboard.kicad_pcb",
-        ]
-        for name in expected_in_keyboard_dir:
-            assert f"keyboard/{name}" in files_in_zip
-
-        with result.open("logs/kbplacer.log") as log_file:
-            assert_kicad_log(log_file, layout_file)
+    layout_test_steps(selenium, layout_file, download_dir)
 
 
 def test_incorrect_layout_expect_error_window(tmpdir, selenium):
