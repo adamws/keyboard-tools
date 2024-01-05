e14f7aa712129105eda47822f0956f073ae729c9
adamws <adamws@users.noreply.github.com>
2024-01-05 20:35:37 +0100

    Add layout sanitize and sort stage

--- kicad-api/src/kicad.py
+++ kicad-api/src/kicad.py
@@ -60,53 +60,13 @@ def prepare_project(project_full_path, project_name, switch_library):
             f.write(result)
 
 
-def generate_netlist_from_layout(
-    project_full_path,
-    layout_file,
-    project_name,
-    switch_library,
-    switch_footprint,
-    controller_circuit,
-):
-    project_libs = "/usr/share/kicad/library"
-    with open(layout_file) as f:
-        json_layout = json.loads(f.read())
-        build_circuit(
-            json_layout,
-            switch_library=switch_library,
-            switch_footprint=switch_footprint,
-            diode_footprint="D_SOD-323F",
-            additional_search_path=project_libs,
-            controller_circuit=controller_circuit == "ATmega32U4",
-        )
-    generate_netlist(f"{project_full_path}/{project_name}.net")
-
-
-def generate_pcb_file(project_full_path, project_name):
-    input_file = f"{project_full_path}/{project_name}.net"
-    output_file = os.path.splitext(input_file)[0] + ".kicad_pcb"
-    kinet2pcb(
-        input_file,
-        output_file,
-        [
-            "/usr/share/kicad/footprints",
-            f"{project_full_path}/libs/keyswitch-kicad-library/footprints",
-        ],
-    )
-
-
-def run_element_placement(project_full_path, project_name, layout_file, settings):
-    pcb_path = f"{project_full_path}/{project_name}.kicad_pcb"
-
+def run_element_placement(pcb_path, layout, settings):
     diode = ElementInfo("D{}", PositionOption.DEFAULT, DEFAULT_DIODE_POSITION, "")
     route_switches_with_diodes = settings["routing"] == "Full"
     route_rows_and_columns = settings["routing"] == "Full"
     additional_elements = [
         ElementInfo("ST{}", PositionOption.CUSTOM, ZERO_POSITION, "")
     ]
-
-    with open(layout_file) as f:
-        layout = json.load(f)
 
     board = pcbnew.LoadBoard(pcb_path)
 
@@ -131,8 +91,7 @@ def run_element_placement(project_full_path, project_name, layout_file, settings
     pcbnew.SaveBoard(pcb_path, board)
 
 
-def add_edge_cuts(project_full_path, project_name):
-    pcb_path = f"{project_full_path}/{project_name}.kicad_pcb"
+def add_edge_cuts(pcb_path):
     try:
         board = pcbnew.LoadBoard(pcb_path)
         positions = [
@@ -169,13 +128,12 @@ def add_edge_cuts(project_full_path, project_name):
         raise Exception(msg) from err
 
 
-def generate_render(project_full_path, project_name):
-    pcb_path = f"{project_full_path}/{project_name}.kicad_pcb"
-
+def generate_render(pcb_path: Path):
     # render is performed on copy of pcb from which all parts outside board edge
     # were removed. This is due to microcontroller circuit which may be present
     # but its placement is outside board outline
-    pcb_for_render = f"{project_full_path}/{project_name}_render.kicad_pcb"
+    project_full_path = Path(pcb_path).parent
+    pcb_for_render = pcb_path.with_stem("render")
 
     shutil.copyfile(pcb_path, pcb_for_render)
 
@@ -210,7 +168,7 @@ def generate_render(project_full_path, project_name):
     image = plotter.plot()
     image.write(f"{project_full_path}/../logs/back.svg")
 
-    for f in glob.glob(f"{project_full_path}/*_render*"):
+    for f in glob.glob(f"{project_full_path}/render*"):
         os.remove(f)
 
 
@@ -226,6 +184,39 @@ def configure_loggers(log_path):
     kbplacer_logger.propagate = False
 
 
+def is_key_label_valid(label):
+    if label and re.match(r"^[0-9]+,[0-9]+$", label):
+        return True
+    else:
+        return False
+
+
+def sanitize_keys(keys):
+    """Make sure that labels are correct and do row-column sorting
+    Some layouts, especially ergo keyboards can have weird key ordering,
+    since we are requiring row/column assignments with labels,
+    we can easily sort it so the annotations of added elements are more natural
+    """
+    for key in keys:
+        labels = key["labels"]
+        if not labels:
+            msg = "Key labels missing"
+            raise RuntimeError(msg)
+
+        # be forgiving, remove all white spaces to fix simple mistakes:
+        labels[0] = re.sub(r"\s+", "", str(labels[0]), flags=re.UNICODE)
+        if not is_key_label_valid(labels[0]):
+            msg = (
+                f"Key label invalid: '{labels[0]}' - "
+                "label needs to follow 'row,column' format, for example '1,2'"
+            )
+            raise RuntimeError(msg)
+
+    def sort_key(item):
+        return tuple(map(int, item["labels"][0].split(",")))
+    keys.sort(key=sort_key)
+
+
 def new_pcb(task_id, task_request, update_state_callback):
     update_state_callback(0)
 
@@ -233,8 +224,6 @@ def new_pcb(task_id, task_request, update_state_callback):
     settings = task_request["settings"]
 
     switch_library = settings["switchLibrary"]
-    switch_footprint = settings["switchFootprint"]
-    controller_circuit = settings["controllerCircuit"]
 
     project_name = layout["meta"]["name"]
     project_name = "keyboard" if project_name == "" else project_name
@@ -249,31 +238,43 @@ def new_pcb(task_id, task_request, update_state_callback):
     update_state_callback(10)
     prepare_project(project_full_path, project_name, switch_library)
 
+    sanitize_keys(layout["keys"])
+
+    pcb_file = f"{project_full_path}/{project_name}.kicad_pcb"
+    netlist_file = f"{project_full_path}/{project_name}.net"
     layout_file = f"{project_full_path}/{project_name}_layout.json"
     with open(layout_file, "w") as out:
         out.write(json.dumps(layout, indent=2))
 
     update_state_callback(20)
-    generate_netlist_from_layout(
-        project_full_path,
-        layout_file,
-        project_name,
-        switch_library,
-        switch_footprint,
-        controller_circuit,
+    build_circuit(
+        layout,
+        switch_library=settings["switchLibrary"],
+        switch_footprint=settings["switchFootprint"],
+        diode_footprint="D_SOD-323F",
+        additional_search_path="/usr/share/kicad/library",
+        controller_circuit=settings["controllerCircuit"] == "ATmega32U4",
     )
+    generate_netlist(netlist_file)
 
     update_state_callback(30)
-    generate_pcb_file(project_full_path, project_name)
+    kinet2pcb(
+        netlist_file,
+        pcb_file,
+        [
+            "/usr/share/kicad/footprints",
+            f"{project_full_path}/libs/keyswitch-kicad-library/footprints",
+        ],
+    )
 
     update_state_callback(40)
-    run_element_placement(project_full_path, project_name, layout_file, settings)
+    run_element_placement(pcb_file, layout, settings)
 
     update_state_callback(50)
-    add_edge_cuts(project_full_path, project_name)
+    add_edge_cuts(pcb_file)
 
     update_state_callback(60)
-    generate_render(project_full_path, project_name)
+    generate_render(Path(pcb_file))
 
     update_state_callback(80)
     shutil.make_archive(task_id, "zip", task_id)
