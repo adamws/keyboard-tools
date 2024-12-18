9661109e24b03a1ea9707b89a8e23fbc752bc668
adamws <adamws@users.noreply.github.com>
2024-12-18 21:06:49 +0100

    Remove unused imports

--- tests/e2e-tests/test_layout_uploads.py
+++ tests/e2e-tests/test_layout_uploads.py
@@ -6,8 +6,6 @@ import re
 import time
 import zipfile
 
-from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
