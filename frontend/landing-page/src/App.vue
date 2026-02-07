b59bd26ed923a4ede2d7f458e5985bc400b23a09
adamws <adamws@users.noreply.github.com>
2026-02-07 13:29:17 +0100

    Update landing page

--- frontend/landing-page/src/App.vue
+++ frontend/landing-page/src/App.vue
@@ -3,18 +3,13 @@
 
 <template>
   <div class="projects-container">
-    <a href="https://editor.keyboard-tools.xyz" style="text-decoration: none; color: inherit;">
-      <div class="project">
-        <p class="project-name">Layout Editor</p>
+    <a href="https://editor.keyboard-tools.xyz">
+      <div class="project size-2u">
+        <p class="project-name">Layout Editor with Plate and PCB generators</p>
       </div>
     </a>
-    <a href="https://editor.keyboard-tools.xyz" style="text-decoration: none; color: inherit;">
-      <div class="project">
-        <p class="project-name">PCB Generator</p>
-      </div>
-    </a>
-    <a href="https://github.com/adamws/kicad-kbplacer" style="text-decoration: none; color: inherit;">
-      <div class="project">
+    <a href="https://github.com/adamws/kicad-kbplacer">
+      <div class="project size-1u">
         <p class="project-name">KiCad Plugin</p>
       </div>
     </a>
@@ -25,13 +20,15 @@
 .projects-container {
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: center;
+}
+.projects-container > a {
+  text-decoration: none;
+  color: inherit;
 }
 .project {
   border: 10px solid;
   border-color: #00ab91;
-  width: 250px;
-  height: 250px;
   margin: 20px;
   display: flex;
   justify-content: center;
@@ -44,5 +41,19 @@
   color: black;
   font-size: 40px;
   text-align: left;
+}
+.size-1u {
+  width: 250px;
+  height: 250px;
+}
+.size-2u {
+  width: 500px;
+  height: 250px;
+}
+@media (max-width: 624px) {
+  .size-2u {
+    width: 250px;
+    height: 500px;
+  }
 }
 </style>
